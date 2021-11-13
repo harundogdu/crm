39c2b535428de05ad4b69da5d044e25f4137b34a
Harun Doğdu <harundogdu06@gmail.com>
2021-11-13 23:56:35 +0300

    laravele react entegresi yapıldı

---
+++ resources/js/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const Index = () => {
+    return (
+        <>
+            <div>Index Sayfası</div>
+        </>
+    );
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
